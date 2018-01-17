5f26d2045e923db8d1a36fa4e6db9130781a863e
Blake Conrad <bmconrad@iupui.edu>
2018-01-17 12:34:43 -0600

    particular / subset of attributes available

--- Package/Test/test.R
+++ Package/Test/test.R
@@ -1,18 +1,29 @@
 # https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
 
-# 1. make the algParams as flexibile as possible.
-# 2. fix sensitivity bug on border cases
 
-getwd()
-setwd("/Users/bmc/Desktop/madm/Package")
-
+setwd("C:/Users/1517766115.CIV/Desktop/Topics")
 source("Functions/FileIO.R")
 source("Functions/Algorithms.R")
 source("Functions/Sensitivity.R")
 dm <- read.data.matrix("Data/maut_validate.csv", header=TRUE)
 
 names(dm)
-FinalDB <- sensitivity(data=dm) #optional
+FinalDB <- sensitivity(data=dm,
+                       step=.1,
+                       attr="Sq.Foot",
+                       algs=c("TOPSIS", "MAUT"),
+                       algParams=list(MAUT=list(scales=list("linear", "linear", "exponential"))),
+                       verbose=FALSE) #optional
+
+FinalDB
+#sensitivity <- function(data=c(), 
+#                        algs=c(),
+#                        algParams=c(),
+#                        step=0.01,
+#                        attr=c(), 
+#                        splitPercentages="uniform", 
+#                        verbose=FALSE){
+
 FinalDB
 summary(FinalDB)
 FinalDB[nrow(FinalDB),]
@@ -61,5 +72,6 @@ write.csv(FinalDB, "final.csv")
 #fdb <- sensitivity(data=dm, algs=c("MAUT", "ELECTRE"), algsParams=c(list(),list()), verbose=TRUE)
 #fdb <- sensitivity(data=dm, attr=c("x1","x2"),algs=c("TOPSIS","MAUT"), algsParams=c(list(),list(scales=c("linear","logarithmic","exponential"))),verbose=TRUE)
 #write.csv(FinalDB,"test.csv")
+
 
 
